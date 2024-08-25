f6cb667c64bd152265b147f9fcdb693aaf60db0d
Alexis Gallagher <alexis@alexisgallagher.com>
2024-08-25 12:49:00 -0700

    Extracts parse tests into data

--- tests/test_parse_dependencies.py
+++ tests/test_parse_dependencies.py
@@ -1,30 +1,43 @@
+import os, tempfile, textwrap
 import pytest
-import os, tempfile
 from pythonrunscript.pythonrunscript import parse_dependencies
 
-def test_parse_dependencies_basic():
-    # Create a temporary file with some test content
-    test_content = """
+
+tests: list[tuple[str, tuple[str, str, str]]] = [
+    (
+    """
     # ```requirements.txt
     # requests==2.26.0
     # ```
-    
-    print("Hello, world!")
-    """
-    p = os.path.join( tempfile.gettempdir(), "test_script.py" )    
-    with open(p, 'w') as f:
-        f.write(test_content)
-    
-    try:
-        # Call the function
-        result = parse_dependencies(p)
-        
-        # Assert the expected results
-        assert isinstance(result, tuple)
-        assert len(result) == 4
-        assert result[1] == "requests==2.26.0\n".strip()  # pip requirements
-        assert result[2] == ""  # conda env
-        assert result[3] == ""  # conda specs
-    finally:
-        # Clean up the temporary file
-        os.remove(p)
+    """,
+        (
+        """
+        requests==2.26.0
+        """,
+        "",
+        "")
+     ),
+]
+f = lambda s:(textwrap.dedent(s)).strip()
+tests = [(f(a),(f(b),f(c),f(d))) for (a,(b,c,d)) in tests]
+del f
+
+def test_parse_dependencies_basic():
+    for (input,(expected_pip_val,expected_conda_env,expected_conda_specs)) in tests:
+        p = os.path.join( tempfile.gettempdir(), "test_script.py" )    
+        with open(p, 'w') as f:
+            f.write(input)
+        try:
+            # Call the function
+            result = parse_dependencies(p)
+            
+            # Assert the expected results
+            assert isinstance(result, tuple)
+            assert len(result) == 4
+            (_, out_pip, out_conda_env, out_conda_specs) = result
+            assert out_pip == expected_pip_val
+            assert out_conda_env == expected_conda_env
+            assert out_conda_specs == expected_conda_specs
+        finally:
+            # Clean up the temporary file
+            os.remove(p)
